67344b9edb0e238653e9a3bd82323fe725d21cc0
Gaurab <gaurabkus781@gmail.com>
2025-06-26 20:36:49 +0530

    use persist and udpate zustand store to add localstorage

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,27 +1,32 @@
-import { create } from "zustand";
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface Recipe {
-  id: number;
-  name: string;
-  ingredients: string[];
-  instructions: string;
+  id: number
+  name: string
+  ingredients: string[]
+  instructions: string
 }
 
 interface RecipeStore {
-  recipes: Recipe[];
-  addRecipe: (recipe: Recipe) => void;
-  removeRecipe: (id: number) => void;
+  recipes: Recipe[]
+  addRecipe: (recipe: Recipe) => void
+  removeRecipe: (id: number) => void
 }
 
-export const useStore = create<RecipeStore>((set) => ({
-  recipes: [],
-  addRecipe: (recipe) =>
-    set((state) => ({ recipes: [...state.recipes, recipe] })),
-
-  removeRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
-}));
-
-
+export const useStore = create<RecipeStore>()(
+  persist(
+    (set) => ({
+      recipes: [],
+      addRecipe: (recipe) =>
+        set((state) => ({ recipes: [...state.recipes, recipe] })),
+      removeRecipe: (id) =>
+        set((state) => ({
+          recipes: state.recipes.filter((r) => r.id !== id),
+        })),
+    }),
+    {
+      name: 'recipe-storage', // localStorage key
+    }
+  )
+)
